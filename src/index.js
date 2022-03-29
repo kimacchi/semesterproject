8a3c3a31bbf33d8dfe1aa25256bc6add01e31059
Ahmet Ergül <shiftusu2@gmail.com>
2022-03-29 18:38:34 +0300

    fixed login page issues with firefox

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "normalize.css";
 import App from './App';
 import "./index.css";
 import {Provider} from "react-redux";
 import {store} from "./store/store";
+
 
 ReactDOM.render(
   <Provider store={store}>
