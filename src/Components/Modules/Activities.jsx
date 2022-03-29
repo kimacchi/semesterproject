9ae513f7813de7782fd46c095c18cbe6b176a4b9
Ahmet Ergül <shiftusu2@gmail.com>
2022-03-29 19:15:25 +0300

    added modal for the activity list, currently there is no content in it.

--- src/Components/Modules/Activities.jsx
+++ src/Components/Modules/Activities.jsx
@@ -4,16 +4,50 @@ import {motion, AnimatePresence, animateVisualElement} from "framer-motion";
 import {useState, useEffect} from "react";
 import { useSelector } from 'react-redux';
 import img from "../../assets/add_button.png";
+import Modal from "react-modal";
 
 
 const axios =require("axios");
 
 
- 
+Modal.defaultStyles.overlay = {
+    ...Modal.defaultStyles.overlay,
+    backgroundColor: "rgba(0,0,0,0.6)",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column"
+}
+
+Modal.defaultStyles.content = {
+    ...Modal.defaultStyles.content,
+    background: "linear-gradient(0deg, rgba(10,83,78,1) 0%, rgba(19,85,78,1) 32%, rgba(16,65,83,1) 64%, rgba(5,22,66,1) 100%)",
+    border: "none",
+    width: "60vw",
+    height: "70vh",
+    top: "15vh",
+    left: "20vw",
+    bottom: 0,
+    right: 0
+}
+
+console.log(Modal.defaultStyles)
+
+Modal.setAppElement("#root")
 
 const Activities = () => {
-    const [activityState, setState] = useState({activities: [], currentId: -10})
+    const [activityState, setState] = useState({activities: [], currentId: -10, modalIsOpen: false})
     const currentId = useSelector((state)=>state.currentUser.userId);
+
+    const openModal = ()=>{
+        setState(prev => ({...prev, modalIsOpen: true}))
+    } 
+
+    const closeModal = ()=>{
+        setState(prev => ({...prev, modalIsOpen: false}))
+    } 
+
+
     useEffect(()=>{
         setState(prev=>({...prev, currentId}))
     }, [currentId])
@@ -31,11 +65,17 @@ const Activities = () => {
             console.log(timeA);
             return timeA > timeB ? 1 : -1;
             })}))
-    }, []);
+    }, [activityState.activities]);
   return (
     <motion.div
         className='activities-main-div'
     >
+        <Modal
+            isOpen={activityState.modalIsOpen}
+            onRequestClose={closeModal}
+        >
+
+        </Modal>
         <p>Approaching Activities</p>
         <motion.div
             className='activities__list'
@@ -43,8 +83,8 @@ const Activities = () => {
             {activityState.activities.length === 0 ? 
                 <div className='activities__empty-list'>
                     <p>Seems like you have no activities. Add some!</p>
-                    <div className='no-activity__add-button'>
-                        <img src={img}></img>
+                    <div className='no-activity__add-button' onClick={openModal}>
+                        <img src={img} alt="add item"></img>
                     </div>
                 </div>
                 :
@@ -56,6 +96,9 @@ const Activities = () => {
                             </div>
                         )
                     })}
+                    <div className='no-activity__add-button' onClick={openModal}>
+                        <img src={img} alt="add item" style={{marginTop: 60}}></img>
+                    </div>
                 </div>
             }
         </motion.div>
